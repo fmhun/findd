fe8d65bd22504f59cb69db29e3f47c0272830cac
Florian Mhun <florian.mhun@gmail.com>
2012-11-10 13:57:42 +0100

    change testrunner for a better visualization

--- unittest/testrunner.cc
+++ unittest/testrunner.cc
@@ -1,3 +1,4 @@
+// ui
 #include "uitest.h"
 #include "commandlinetest.h"
 #include "configtest.h"
@@ -13,16 +14,29 @@
 
 #include "apptest.h"
 
+#include <cppunit/BriefTestProgressListener.h>
+#include <cppunit/CompilerOutputter.h>
 #include <cppunit/extensions/TestFactoryRegistry.h>
-#include <cppunit/ui/text/TestRunner.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestResultCollector.h>
+#include <cppunit/TestRunner.h>
 
-int main(void) {
-	CppUnit::TextUi::TestRunner runner;
-	
-	CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
-	runner.addTest( registry.makeTest() );
-	  
-	bool wasSuccessful = runner.run("", false);
-	
-	return wasSuccessful;
+int main () {
+  CPPUNIT_NS::TestResult controller;
+
+  CPPUNIT_NS::TestResultCollector result;
+  controller.addListener( &result );        
+
+  CPPUNIT_NS::BriefTestProgressListener progress;
+  controller.addListener( &progress );      
+
+  CPPUNIT_NS::TestRunner runner;
+  runner.addTest( CPPUNIT_NS::TestFactoryRegistry::getRegistry().makeTest() );
+  runner.run( controller );
+
+  CPPUNIT_NS::CompilerOutputter outputter( &result, CPPUNIT_NS::stdCOut() );
+  outputter.write(); 
+
+  return result.wasSuccessful() ? 0 : 1;
 }
+
