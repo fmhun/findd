946d251b2f08e8c90055ecb9c92b376092abf2ed
Florian Mhun <florian.mhun@gmail.com>
2012-12-14 11:30:56 +0100

    fix global.h

--- src/global.h
+++ src/global.h
@@ -4,9 +4,10 @@
 #include <string>
 #include <vector>
 #include <list>
-#include "file.h"
 #include <iostream>
 #include <sstream>
+
+#include "file.h"
 
 typedef struct std::list<findd::File> file_list;
 typedef struct std::vector<findd::File> duplicate;
@@ -25,7 +26,7 @@ struct env_t {
   std::vector<std::string> directories;  
   std::string in_scan_file;
   std::string out_scan_file;
-  filter_t filter;
+  struct filter_t filter;
   bool display_stats;
   bool remove;
 };
